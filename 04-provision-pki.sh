4e6aee13839ac3bf18bbe5ce59e43aea284a4b9c
TempleCloud <tim.langford@gmail.com>
2018-06-05 23:55:35 +0100

    Added CA, admin, and kubblet certificate generation.

--- 04-provision-pki.sh
+++ 04-provision-pki.sh
@@ -24,6 +24,15 @@ function check-prerequisites() {
   fi
 }
 
+# Provisioning CA *************************************************************
+#
+
+# Provision a Certificate Authority that can be used to generate additional 
+# TLS certificates:
+#   * CA configuration files
+#   * certificate
+#   * private key
+#
 function create-certificate-authority() {
   # Create ca-config.
   cat > ca-config.json <<EOF
@@ -69,6 +78,103 @@ function delete-certificate-authority() {
   rm -f ca-key.pem ca.csr ca.pem
   rm -f ca-csr.json 
   rm -f ca-config.json 
+}
+
+# Provision Admin Client Certificates *****************************************
+#
+
+# Generate client and server certificates for each Kubernetes component and a 
+# client certificate for the Kubernetes admin user.
+
+function create-admin-certs() {
+  # Generate config file
+  cat > admin-csr.json <<EOF
+{
+  "CN": "admin",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:masters",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+  # Generate the admin certificates and private key using CA.
+  cfssl gencert \
+    -ca=ca.pem \
+    -ca-key=ca-key.pem \
+    -config=ca-config.json \
+    -profile=kubernetes \
+    admin-csr.json | cfssljson -bare admin
+}
+
+function delete-admin-certs() {
+  rm -f admin-key.pem admin.csr admin.pem  
+  rm -f admin-csr.json 
+}
+
+# Provision Kublet Client Certificates ****************************************
+#
+
+# Kubernetes uses a special-purpose authorization mode called Node Authorizer, 
+# that specifically authorizes API requests made by Kubelets. In order to be 
+# authorized by the Node Authorizer, Kubelets must use a credential that 
+# identifies them as being in the system:nodes group, with a username of 
+# system:node:<nodeName>. 
+#
+# Create a certificate for each Kubernetes worker node hat meets the Node 
+# Authorizer requirements.
+#
+# * https://kubernetes.io/docs/reference/access-authn-authz/node/
+# * https://kubernetes.io/docs/concepts/overview/components/#kubelet
+
+function create-kublet-certs() {
+  for instance in worker-0 worker-1 worker-2; do
+    cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+    EXTERNAL_IP=$(gcloud compute instances describe ${instance} \
+      --format 'value(networkInterfaces[0].accessConfigs[0].natIP)')
+
+    INTERNAL_IP=$(gcloud compute instances describe ${instance} \
+      --format 'value(networkInterfaces[0].networkIP)')
+
+    cfssl gencert \
+      -ca=ca.pem \
+      -ca-key=ca-key.pem \
+      -config=ca-config.json \
+      -hostname=${instance},${EXTERNAL_IP},${INTERNAL_IP} \
+      -profile=kubernetes \
+      ${instance}-csr.json | cfssljson -bare ${instance}
+  done
+}
+
+function delete-kublet-certs() {
+  rm -f worker-*.csr worker-*.pem worker-*-key.pem 
+  rm -f worker-*-csr.json 
 }
 
 # Main ************************************************************************
