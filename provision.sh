99c0b8bf1846c8df380773cf73d8c063df2c4334
TempleCloud <tim.langford@gmail.com>
2018-06-07 20:44:15 +0100

    Added encryption key generation.

--- provision.sh
+++ provision.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${DIR}/03-provision-infra.sh" ""
 source "${DIR}/04-provision-pki.sh" ""
 source "${DIR}/05-provision-kubeconfigs.sh" ""
+source "${DIR}/06-provision-encryption.sh" ""
 
 # K8s Cluster *****************************************************************
 #
@@ -12,12 +13,14 @@ function create-k8s() {
   create-infra
   create-certs
   create-kubeconfigs
+  create-encryption
 }
 
 function delete-k8s() {
   delete-kubeconfigs
   delete-certs
   delete-infra
+  delete-encryption
 }
 
 # Main ************************************************************************
